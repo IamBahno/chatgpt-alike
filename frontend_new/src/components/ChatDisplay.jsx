ae5016dcf049cd1751a609a0d7f54fbbe47f5122
IamBahno <obahounek@email.cz>
2024-09-22 15:58:58 +0200

    refactor ChatDisplay

--- frontend_new/src/components/ChatDisplay.jsx
+++ frontend_new/src/components/ChatDisplay.jsx
@@ -13,155 +13,97 @@ import { fetchEventSource } from '@microsoft/fetch-event-source';
 // TODO user muze dat messeage a behem toho co se generuje muze odeslat novou a dela to bordel
 const ChatDisplay = ({ conversationEntries, setConversationEntries, genResponse, optionsData }) => {
   const { accessToken, currentChat, setCurrentChat, addChatToList } = useContext(AppContext); 
-  const [eventSource, setEventSource] = useState(null); // State to manage eventSource
 
-  const handleSendMessage = async (userPrompt,chatId) => {
-    // try {
-      const enpoint = "http://localhost:8000/chat/message";
-      // Make a POST request to send the user's prompt to the backend
-      // const response = await axios.post(enpoint, 
-      const newEventSource = await fetchEventSource(enpoint, 
-        { method: "POST",
-          headers: { 
-            Authorization: `Bearer ${accessToken}`, 
-            Accept: "text/event-stream",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ prompt: userPrompt,
-            options: optionsData,
-            chat_id: chatId }),
-          onopen(res) {
-              if (res.ok && res.status === 200) {
-                console.log("Connection made", res);
-              } else if (res.status >= 400 && res.status < 500 && res.status !== 429) {
-                console.log("Client-side error", res);
-              }
-          },
-          onmessage(event) {
-            console.log("Received event data:", event.data);
-            let jsonPart = event.data.split("data: ")[1]
-
-            // Parse the JSON part
-            const parsedData = JSON.parse(jsonPart);
-            if (parsedData.type == "message"){
-              setConversationEntries((prevEntries) => {
-                const updatedEntries = [...prevEntries];
-                const lastEntry = updatedEntries[updatedEntries.length - 1];
-                lastEntry.ai_response += parsedData.data;
-                return updatedEntries;
-              });
-            }            
-            // nakonec prijmu data k chat entries a vytvorim a nastovim novy chat
-            else if (parsedData.type == "final"){
-              // pridam  cost, to musim udelat i kdyz to neni first message
-              setConversationEntries((prevEntries) => {
-                const updatedEntries = [...prevEntries];
-                updatedEntries[updatedEntries.length - 1] = {
-                  ...updatedEntries[updatedEntries.length - 1],
-                  cost : parsedData.data.cost
-                }
-                return updatedEntries;
-              });
-            }
-          },
-          onclose() {
-            console.log("Connection closed by the server");
-          },
-          onerror(err) {
-            console.log("There was an error from server", err);
-          },
+  const handleSendEventSource = async (endpoint,body) => {
+    const eventSource = await fetchEventSource(endpoint, 
+      { method: "POST",
+        headers: { 
+          Authorization: `Bearer ${accessToken}`, 
+          Accept: "text/event-stream",
+          "Content-Type": "application/json",
         },
-      );
-  setEventSource(newEventSource);
-  };
-  const handleSendFirstMessage = async (userPrompt) => {
-      const enpoint = "http://localhost:8000/chat/first_message";
-      // Make a POST request to send the user's prompt to the backend
-      // const response = await axios.post(enpoint, 
-      const newEventSource = await fetchEventSource(enpoint, 
-        { method: "POST",
-          headers: { 
-            Authorization: `Bearer ${accessToken}`, 
-            Accept: "text/event-stream",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ prompt: userPrompt,
-            options: optionsData,
-            }),
-          onopen(res) {
-              if (res.ok && res.status === 200) {
-                console.log("Connection made", res);
-              } else if (res.status >= 400 && res.status < 500 && res.status !== 429) {
-                console.log("Client-side error", res);
-              }
-          },
-          onmessage(event) {
-            console.log("Received event data:",event.data);
-            // # Extract the JSON part
-            let jsonPart = event.data.split("data: ")[1]
+        body: JSON.stringify(body),
+        onopen(res) {
+            if (res.ok && res.status === 200) {
+              console.log("Connection made", res);
+            } else if (res.status >= 400 && res.status < 500 && res.status !== 429) {
+              console.log("Client-side error", res);
+            }
+        },
+        onmessage(event) {
+          console.log("Received event data:", event.data);
+          let jsonPart = event.data.split("data: ")[1]
 
-            // Parse the JSON part
-            const parsedData = JSON.parse(jsonPart);
-            if (parsedData.type == "message"){
-              setConversationEntries((prevEntries) => {
-                const updatedEntries = [...prevEntries];
-                const lastEntry = updatedEntries[updatedEntries.length - 1];
-                lastEntry.ai_response += parsedData.data;
-                return updatedEntries;
-              });
-            }            
-            // nakonec prijmu data k chat entries a vytvorim a nastovim novy chat
-            else if (parsedData.type == "final"){
-              // pridam  cost, to musim udelat i kdyz to neni first message
-              setConversationEntries((prevEntries) => {
-                const updatedEntries = [...prevEntries];
-                updatedEntries[updatedEntries.length - 1] = {
-                  ...updatedEntries[updatedEntries.length - 1],
-                  cost : parsedData.data.cost
-                }
-                return updatedEntries;
-              });
+          // Parse the JSON part
+          const parsedData = JSON.parse(jsonPart);
+          if (parsedData.type == "message"){
+            setConversationEntries((prevEntries) => {
+              const updatedEntries = [...prevEntries];
+              const lastEntry = updatedEntries[updatedEntries.length - 1];
+              lastEntry.ai_response += parsedData.data;
+              return updatedEntries;
+            });
+          }            
+          // nakonec prijmu data k chat entries a vytvorim a nastovim novy chat
+          else if (parsedData.type == "final"){
+            // pridam  cost, to musim udelat i kdyz to neni first message
+            setConversationEntries((prevEntries) => {
+              const updatedEntries = [...prevEntries];
+              updatedEntries[updatedEntries.length - 1] = {
+                ...updatedEntries[updatedEntries.length - 1],
+                cost : parsedData.data.cost
+              }
+              return updatedEntries;
+            });
+
+            // it this is first message, set the current chat and add it to the chat list
+            if(!currentChat){
               //vytvorim novej chat
               const newChat = {id : parsedData.data.chat_id,
-                              title : parsedData.data.chat_title};
+                      title : parsedData.data.chat_title};
               //add him to the chat list
               addChatToList(newChat);
               //nastavim jako current (set current chat)
               setCurrentChat(newChat);
             }
-          },
-          onclose() {
-            console.log("Connection closed by the server");
-          },
-          onerror(err) {
-            console.log("There was an error from server", err);
-          },
+          }
         },
-      );
-  setEventSource(newEventSource);
-  };
-
-  // Cleanup function to close the eventSource when the component unmounts
-  useEffect(() => {
+        onclose() {
+          console.log("Connection closed by the server");
+        },
+        onerror(err) {
+          console.log("There was an error from server", err);
+        },
+      },
+    );
+    // Cleanup function to close the eventSource when the component unmounts
     return () => {
       if (eventSource) {
         eventSource.close();
       }
     };
-  }, [eventSource]);
+  }
 
-  // When chatData updates, trigger sending message if the last entry is unsent
   useEffect(() => {
+  // When chatData updates, trigger sending message if the last entry is unsent
     const lastEntry = conversationEntries[conversationEntries.length - 1];
     if (lastEntry && lastEntry.ai_response === '') {
       if(conversationEntries.length === 1){
-        handleSendFirstMessage(lastEntry.user_prompt);
+        // handleSendFirstMessage(lastEntry.user_prompt);
+        const endpoint = "http://localhost:8000/chat/first_message";
+        const body = { prompt: lastEntry.user_prompt, options: optionsData };
+        handleSendEventSource(endpoint, body);
       }
       else{
-        handleSendMessage(lastEntry.user_prompt, currentChat.id);
+        // handleSendMessage(lastEntry.user_prompt, currentChat.id);
+        const endpoint = "http://localhost:8000/chat/message";
+        const body = { prompt: lastEntry.user_prompt, options: optionsData, chat_id: currentChat.id };
+        handleSendEventSource(endpoint, body);
       }
     }
-  }, [genResponse]);  
+
+  },[genResponse]);
+
 
 
     // Check if conversationEntries is missing or empty
@@ -184,7 +126,7 @@ const ChatDisplay = ({ conversationEntries, setConversationEntries, genResponse,
           </div>
           <div className="entry-details">
             <span className="cost">
-              <strong>Cost:</strong> ${entry.cost.toFixed(4)} {/* Format cost to 2 decimal places */}
+              <strong>Cost:</strong> ${entry.cost.toFixed(4)} {/* Format cost to 4 decimal places */}
             </span>
             <span className="timestamp">
               <strong>Time:</strong> {new Date(entry.time).toLocaleString()} {/* Format time nicely */}
